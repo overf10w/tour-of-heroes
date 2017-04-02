a2702407bdf1696c6d4fbf7ddb0c54956c2deef0
pvvner <creedge@yandex.ru>
2017-04-03 01:38:55 +0300

    Doing toh-5(6)-Routing. Add Back button to HeroDetailComponent

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,16 +8,7 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
 
@@ -34,4 +25,7 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
