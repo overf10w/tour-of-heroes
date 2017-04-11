4c08590c7af84962a9a944d3bb478cfb573f674f
pwznr <pvvner@yandex.ru>
2017-04-11 14:36:42 +0300

    Add the ability to add heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,14 @@ export class HeroService {
 
   constructor(private http: Http) { }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
                .toPromise()
